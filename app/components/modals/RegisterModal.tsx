66f25c56cdbc4aaa3ead446b923a3102e57eb0d2
rizkyr99 <ramarizdev@gmail.com>
2023-04-07 09:20:28 +0700

    added register functionality

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -67,6 +67,7 @@ const RegisterModal = () => {
       <Input
         id='password'
         label='Password'
+        type='password'
         disabled={isLoading}
         register={register}
         errors={errors}
